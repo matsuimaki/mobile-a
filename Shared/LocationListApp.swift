d997abb6845afd67ac3fc0671dc1cacbf254e979
Maki Matsui <e195729@ie.u-ryukyu.ac.jp>
2021-10-19 17:28:21 +0900

    Initial Commit

---
+++ Shared/LocationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationListApp.swift
+//  Shared
+//
+//  Created by Matsui Maki on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct LocationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
