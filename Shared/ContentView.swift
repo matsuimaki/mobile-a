269615d72cd5af3b9a57addd257b9cc1606c0d79
Maki Matsui <e195729@ie.u-ryukyu.ac.jp>
2021-11-30 17:10:08 +0900

    third commit(scrollview worked for a second)

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,7 +5,6 @@
 //  Created by Matsui Maki on 2021/10/19.
 //
 //全体
-
 import SwiftUI
 
 class Timeline:ObservableObject{
@@ -48,60 +47,60 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        GeometryReader{geometry in
-            ZStack(alignment: .leading){
-                //メインコンテンツ
-                NavigationView{
-                    ZStack{
-                        ScrollView(.vertical){
-                            VStack{
-                                TimelineView(timelines: $timelines)
-                            }
-                        }
-                        //メニューを押されたら背景をグレーアウトする
-                        Color.gray.opacity(
-                            Double((self.closeOffset  - self.currentOffset)/self.closeOffset) - 0.4
-                        )
+        ZStack(alignment: .leading){
+            //メインコンテンツ
+            NavigationView{
+                ZStack{
+                    HStack {
+                        Spacer()
                     }
-                    //タイトル
-                    .navigationBarTitle("Location History", displayMode: .inline)
-                    //ハンバーガーメニューボタンの配置
-                    .navigationBarItems(leading: Button(action: {
-                        self.currentOffset = self.openOffset
-                    }){
-                        Image("Hamburger2")
-                            .renderingMode(.original)
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                    })
-                }
-                //スライドメニューの出現設定
-                HamburgermenuView()
-                    .background(Color.white)
-                    .frame(width: geometry.size.width * 0.7)
-                    .edgesIgnoringSafeArea(.bottom)
-                    .onAppear(perform: {
-                        self.currentOffset = geometry.size.width * -1
-                        self.closeOffset = self.currentOffset
-                        self.openOffset = .zero
-                    })
-                    .offset(x: self.currentOffset)
-                    .animation(.default)
-            }
-            .gesture(DragGesture(minimumDistance: 5)
-                        //スライドメニューを消すジェスチャー
-                        .onEnded{value in
-                                self.currentOffset = self.closeOffset
+                    ScrollView(.vertical){
+                        VStack{
+                            TimelineView(timelines: $timelines)
                         }
-            )
-        }
+                    }
+                    //メニューを押されたら背景をグレーアウトする
+                    Color.gray.opacity(
+                        Double((self.closeOffset  - self.currentOffset)/self.closeOffset) - 0.4
+                    )
+                }
+                //タイトル
+                .navigationBarTitle("Location History", displayMode: .inline)
+                //ハンバーガーメニューボタンの配置
+                .navigationBarItems(
+                    leading: Button(
+                        action: {
+                            self.currentOffset = self.openOffset
+                        })
+                    {
+                        Image("Hamburger2")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    }
+                )
+            }
+            //スライドメニューの出現設定
+            HamburgermenuView()
+                .background(Color.white)
+                .frame(width: UIScreen.main.bounds.width*0.7)
+                .edgesIgnoringSafeArea(.bottom)
+                .onAppear(perform: {
+                    self.currentOffset = UIScreen.main.bounds.width * -1
+                    self.closeOffset = self.currentOffset
+                    self.openOffset = .zero
+                })
+                .offset(x: self.currentOffset)
+                .animation(.default)
+        }.gesture(DragGesture(minimumDistance: 5)
+                    .onEnded{value in
+                        self.currentOffset = self.closeOffset
+        })
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
--- Shared/TimelineView.swift
+++ Shared/TimelineView.swift
@@ -15,47 +15,48 @@ struct TimelineView: View {
     
     var body: some View {
         ZStack{
-            //ScrollView(.vertical){
-                VStack(spacing:15){
+            VStack(spacing:15){
                     ForEach(self.timelines, id: \.id){ (timeline) in
-                        Button(action:{
-                            withAnimation{
-                                showingPopUp = true
-                                //押されたボタンのidを取得するコードを追加
-                            }
-                        }, label:{
-                            HStack(){
-                                ZStack{
-                                    Circle()
-                                        .frame(width:30, height:30, alignment: .center)
-                                        .foregroundColor(.blue)
-                                    Text(timeline.day)
-                                        .fontWeight(.bold)
+                        Button(
+                            action:{
+                                withAnimation{
+                                    showingPopUp = true
                                 }
-                                .padding(.leading,10)
-                                Text(timeline.location)
-                                    .padding()
-                                Spacer()
-                                Divider()
-                                    .padding(10)
-                                Group{
-                                    Text(timeline.time_h) + Text(timeline.time_m)
-                                }
-                                .padding(.trailing)
                             }
-                            .frame(width: UIScreen.main.bounds.width-50, height:50, alignment: .leading)
-                            .background(Color.yellow)
-                            .cornerRadius(10)
-                            .id(timeline.id)
-                        })
+                            ,label:{
+                                HStack(){
+                                    ZStack{
+                                        Circle()
+                                            .frame(width:30, height:30, alignment: .center)
+                                            .foregroundColor(.blue)
+                                        Text(timeline.day)
+                                            .fontWeight(.bold)
+                                    }
+                                    .padding(.leading,10)
+                                    Text(timeline.location)
+                                        .padding()
+                                    Spacer()
+                                    Divider()
+                                        .padding(10)
+                                    Group{
+                                        Text(timeline.time_h) + Text(timeline.time_m)
+                                    }
+                                    .padding(.trailing)
+                                }
+                                .frame(width: UIScreen.main.bounds.width-50, height:50, alignment: .leading)
+                                .background(Color.yellow)
+                                .cornerRadius(10)
+                                .id(timeline.id)
+                            }
+                        )
                         .buttonStyle(PlainButtonStyle())
                     }
                 }
-            //}
             if showingPopUp{
                 Color.gray.opacity(0.8)
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                 DetailView(isPresent: $showingPopUp)
-                //押されたボタンのidを渡すコードを追加
+                    .position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/3)
             }
         }
     }
